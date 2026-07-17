9e0b9d7295784ed2588feff76b36c2bc20981816
merafiulhoque <me.rafiulhoque@gmail.com>
2026-07-17 08:15:58 +0530

    signout

--- src/features/auth/signout/signout.controller.ts
+++ src/features/auth/signout/signout.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 
 export async function signOut(req: Request, res: Response){
     return res
-                .status(200)
                 .clearCookie("token")
+                .status(200)
                 .json({
                     success: true,
                     message: "Logout Successfull..."
